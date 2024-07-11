c3753dc372bd384a8f7d864624af8e855cdc2e47
md <marceldiomare@gmail.com>
2024-07-11 21:16:28 +0300

    update settings.py add db name

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_task',
+        'NAME': 'django_task_catalog',
         'USER': 'postgres',
         'PASSWORD': '0013',
     }
