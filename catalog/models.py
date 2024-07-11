02c7e2e534d1d5e00f7442cefec68bcc1685c814
md <marceldiomare@gmail.com>
2024-07-11 23:20:38 +0300

    added models in models.py, poetry add black

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product = models.CharField(
+        max_length=100,
+        verbose_name="Наименование",
+        help_text="Введите наименование товара",
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание товара"
+    )
+    image = models.ImageField(
+        upload_to="catalog/image",
+        blank=True,
+        null=True,
+        verbose_name="Изображение",
+        help_text="Загрузите изображение товара",
+    )
+    category = models.CharField(
+        max_length=100, verbose_name="Категория", help_text="Введите категорию товара"
+    )
+    price = models.PositiveIntegerField(
+        verbose_name="Цена за покупку", help_text="Введите цену за покупку  товара"
+    )
+    created_at = models.DateTimeField(
+        verbose_name="Дата записи в БД", help_text="Введите дату записи в БД"
+    )
+    updated_at = models.DateTimeField(
+        verbose_name="Дата обновления записи в БД",
+        help_text="Введите дату обновления записи в БД",
+    )
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ["product", "description", "category", "price"]
+
+    def __str__(self):
+        return self.product
+
+
+class Category(models.Model):
+    category = models.CharField(
+        max_length=100,
+        verbose_name="Категория",
+        help_text="Введите наименование категории",
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание",
+        help_text="Введите описание категории",
+    )
+
+    created_at = models.DateTimeField(
+        verbose_name="Дата записи в БД", help_text="Введите дату записи в БД"
+    )
+    updated_at = models.DateTimeField(
+        verbose_name="Дата обновления записи в БД",
+        help_text="Введите дату обновления записи в БД",
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["category", "description"]
+
+    def __str__(self):
+        return self.category
