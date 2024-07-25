9955432c0df9ba4ab47a70d22f83cbaf22252574
md <marceldiomare@gmail.com>
2024-07-25 23:58:53 +0300

    added highlighted the basic template

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import index, contact, card
+from catalog.views import products_list, contact, card
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', products_list, name='home'),
     path('contacts', contact, name='contacts'),
     path('card', card, name='card')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 
+from catalog.models import Product
 
-def index(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        tel = request.POST.get('tel')
-        message = request.POST.get('message')
-        print(f'You have new message from {name}({tel}): {message}')
-    return render(request, 'main/index.html')
 
+# def index(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         tel = request.POST.get('tel')
+#         message = request.POST.get('message')
+#         print(f'You have new message from {name}({tel}): {message}')
+#     return render(request, 'main/index.html')
+
+
+def products_list(requests):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(requests, 'main/products_list.html', context)
 
 def contact(request):
     if request.method == 'POST':
