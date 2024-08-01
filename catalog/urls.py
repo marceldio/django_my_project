9fff0fb25ef91b68642e47ace5d3f5cb2e985d9a
md <marceldiomare@gmail.com>
2024-08-01 21:49:43 +0300

    o7 fbv -> cbv

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, contact, card
+from catalog.views import ProductListView, contact, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
 
-    path('catalog/<int:pk>/', card, name='card'),
+    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='card'),
     path('contacts', contact, name='contacts'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from catalog.models import Product
 
 
 class ProductListView(ListView):
     model = Product
-
+    template_name = ('main/product_list.html')
     # app.name/<model_name>_<action
     # catalog/product_list.html
 
@@ -14,6 +14,17 @@ class ProductListView(ListView):
 #     products = Product.objects.all()
 #     context = {'products': products}
 #     return render(requests, 'main/products_list.html', context)
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = ('main/product_detail.html')
+
+# def card(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     context = {'product': product}
+#     return render(request, 'main/product_detail.html', context)
+
 
 def contact(request):
     if request.method == 'POST':
@@ -23,8 +34,3 @@ def contact(request):
         print(f'You have new message from {name}({tel}): {message}')
     return render(request, 'main/contact.html')
 
-
-def card(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {'product': product}
-    return render(request, 'main/card.html', context)
