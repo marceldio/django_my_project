a0ead02162f0236bafc172054d9ad53bcd5b1629
md <marceldiomare@gmail.com>
2024-08-03 00:33:57 +0300

    added ProductCreateView - got an error TemplateDoesNotExist at /create/

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, contact, ProductDetailView
+from catalog.views import ProductListView, contact, ProductDetailView, ProductCreateView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
 
-    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='card'),
+    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('create/', ProductCreateView.as_view(), name='product_create'),
+
     path('contacts', contact, name='contacts'),
 ]
