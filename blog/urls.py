c9f092aa4010470936bb944f7eb086f62eaede65
md <marceldiomare@gmail.com>
2024-08-05 02:15:28 +0300

    everything is ready, everything works, all that remains is to remove the unnecessary

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from blog.apps import BlogConfig
-from blog.views import BlogCreateView
+from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView
 
 app_name = BlogConfig.name
 
@@ -15,8 +15,8 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path('create/', BlogCreateView.as_view(), name='blog_create'),
-    # path('', BlogListView.as_view(), name='blog_list'),
-    # path('view/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
-    # path('edit/<int:pk>/', BlogUpdateView.as_view(), name='blog_edit'),
-    # path('delete/<int:pk>/', BlogDeleteView.as_view(), name='blog_delete'),
+    path('', BlogListView.as_view(), name='blog_list'),
+    path('view/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('edit/<int:pk>/', BlogUpdateView.as_view(), name='blog_edit'),
+    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='blog_delete'),
 ]
